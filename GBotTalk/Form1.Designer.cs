f7b5faf6323179194bd9f8d6b0d733b9fd4a5fec
nmundar <usermeister2@gmail.com>
2011-07-05 03:04:39 +0200

    Change of lib to jabber.net.

--- GBotTalk/Form1.Designer.cs
+++ GBotTalk/Form1.Designer.cs
@@ -29,11 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtFill = new System.Windows.Forms.TextBox();
+            this.btnListFill = new System.Windows.Forms.Button();
+            this.jabberClient1 = new jabber.client.JabberClient(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(13, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtFill
+            // 
+            this.txtFill.Location = new System.Drawing.Point(12, 42);
+            this.txtFill.Multiline = true;
+            this.txtFill.Name = "txtFill";
+            this.txtFill.Size = new System.Drawing.Size(283, 179);
+            this.txtFill.TabIndex = 1;
+            // 
+            // btnListFill
+            // 
+            this.btnListFill.Location = new System.Drawing.Point(94, 13);
+            this.btnListFill.Name = "btnListFill";
+            this.btnListFill.Size = new System.Drawing.Size(75, 23);
+            this.btnListFill.TabIndex = 2;
+            this.btnListFill.Text = "Fill list";
+            this.btnListFill.UseVisualStyleBackColor = true;
+            this.btnListFill.Click += new System.EventHandler(this.btnListFill_Click);
+            // 
+            // jabberClient1
+            // 
+            this.jabberClient1.AutoReconnect = 30F;
+            this.jabberClient1.AutoStartCompression = true;
+            this.jabberClient1.AutoStartTLS = true;
+            this.jabberClient1.InvokeControl = this;
+            this.jabberClient1.KeepAlive = 30F;
+            this.jabberClient1.LocalCertificate = null;
+            this.jabberClient1.Password = "l0zink4";
+            this.jabberClient1.Server = "gmail.com";
+            this.jabberClient1.User = "usermeister1@gmail.com";
+            this.jabberClient1.OnMessage += new jabber.client.MessageHandler(this.jabberClient1_OnMessage);
+            this.jabberClient1.OnConnect += new jabber.connection.StanzaStreamHandler(this.jabberClient1_OnConnect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 398);
+            this.Controls.Add(this.btnListFill);
+            this.Controls.Add(this.txtFill);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtFill;
+        private System.Windows.Forms.Button btnListFill;
+        private jabber.client.JabberClient jabberClient1;
     }
 }
 
--- GBotTalk/Form1.cs
+++ GBotTalk/Form1.cs
@@ -11,9 +11,44 @@ namespace GBotTalk
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Connect()
+        {
+            jabberClient1.Connect();
+        }
+
+        private void btnConnect_Click(object sender, System.EventArgs e)
+        {
+            Connect();
+        }
+
+        private void btnListFill_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {;
+        }
+
+        private void jabberClient1_OnMessage(object sender, jabber.protocol.client.Message msg)
+        {
+            jabber.protocol.client.Message reply = new jabber.protocol.client.Message(jabberClient1.Document);
+            reply.Body = "Hello!";
+            reply.To = msg.From;
+            jabberClient1.Write(reply);
+            txtFill.Text = msg.Body;
+        }
+
+        private void jabberClient1_OnConnect(object sender, jabber.connection.StanzaStream stream)
+        {
+            txtFill.Text = "Connect";
+        }	
+
     }
 }
